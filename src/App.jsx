44047056e82f3e7d7ee86fcf1b399053b5cf8aab
Asadbek0308 <asashdedpubg@gmail.com>
2026-01-17 14:36:35 +0500

    hgh

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ const App = () => {
     <div className='bg-red-600 text-7xl text-white text-center my-75'>
       Salom Vercel
       <h1>Yangi ozgarish</h1>
+      <div className='bg-blue-600 text-7xl text-green-200 text-center my-75'>
+      Hayr vercel
+      <h1>Yangi ozgarish</h1>
+    </div>
     </div>
   )
 }
