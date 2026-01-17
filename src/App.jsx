179db6c6e0adec58543c7f4ae7896ffc9bf9c3e1
Asadbek0308 <asashdedpubg@gmail.com>
2026-01-17 14:19:35 +0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-red-600 text-7xl text-white text-center my-[300px]'>
+      Salom Vercel
+    </div>
+  )
+}
+
+export default App
